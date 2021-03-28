9d2e0f4b89a983cc9f444895f65e6826d5e37e54
sifat hassan <sifat007.sh7@gmail.com>
2021-03-28 09:19:16 +0600

    menu helper for return menu

---
+++ app/Helpers/MenuHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!function_exists('DummyFunction')) {
+
+    /**
+     * description
+     *
+     * @param
+     * @return
+     */
+    function menu($name)
+    {
+
+        $menu = \App\Models\Menu::where('name',$name)->first();
+        return $menu->menuItems()->with('childs')->get();
+    }
+}
